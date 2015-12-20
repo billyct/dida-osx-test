9e25fa8b45d5580728e0db988abec805795c6502
billyct <billy_allen@126.com>
2015-12-21 05:15:28 +0800

    Initial Commit

---
+++ dida/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  dida
+//
+//  Created by billyct on 15/12/21.
+//  Copyright © 2015年 billyct. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ dida/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  dida
+//
+//  Created by billyct on 15/12/21.
+//  Copyright © 2015年 billyct. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
